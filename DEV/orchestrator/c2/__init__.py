fa1f5e14f8cbd853d1ac1d3d401ec5fab3e53779
Michael Landbo <115590310+L4DK@users.noreply.github.com>
2025-12-23 15:08:06 +0100

    Update __init__.py
    Signed-off-by: Michael Landbo <115590310+L4DK@users.noreply.github.com>

--- DEV/orchestrator/c2/__init__.py
+++ DEV/orchestrator/c2/__init__.py
@@ -1,3 +1,9 @@
+# FilePath: "/DEV/orchestrator/c2/__init__.py"
+# Projekt: Unified Bot Protocol (UBP)
+# Beskrivelse: Eksponerer Command & Control handlers.
+# Author: "Michael Landbo"
+# Date created: "21/12/2025"
+# Version: "v.1.0.0"
 
 from .handler import SecureC2ConnectionHandler
 from .secure_handler import SecureC2Handler
