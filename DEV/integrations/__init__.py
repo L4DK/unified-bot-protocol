2fcc48ca05126aa416a496fa68e0151edbb1dfbe
Michael Landbo <115590310+L4DK@users.noreply.github.com>
2025-12-23 15:14:50 +0100

    Update __init__.py
    Signed-off-by: Michael Landbo <115590310+L4DK@users.noreply.github.com>

---
+++ DEV/integrations/__init__.py
@@ -0,0 +1,8 @@
+# FilePath: "/DEV/integrations/__init__.py"
+# Project: Unified Bot Protocol (UBP)
+# Description: Integrations module initialization.
+# Author: "Michael Landbo"
+# Date created: "21/12/2025"
+# Version: "v.1.0.0"
+
+# This file is intentionally left empty to mark the directory as a Python package.
